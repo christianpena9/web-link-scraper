7137c040244d5c0986065838809daa78aade63b9
Christian Pena <christian.pena@ralphlauren.com>
2020-06-13 22:48:58 -0400

    added python request and beautifulsoup

--- index.js
+++ index.js
@@ -2,17 +2,32 @@ const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const { Pool } = require('pg');
+const { Pool } = require('pg')
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
 });
+const callPython = require('./callPython');
+const spawn = require("child_process").spawn;
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
+    .get('/example', (req, res) => res.render('pages/example'))
+    .get('/callPython', (req, res) => {
+        // spawn new child process to call the python script
+        var process = spawn('python', ['./public/python/fetchURL.py', req.query.param]);
+        // Takes stdout data from script which executed
+        // with arguments and send this data to res object
+        process.stdout.on('data', (data) => {
+            var obj = {
+                data: data.toString()
+            }
+            res.send(JSON.stringify(obj));
+        });
+    })
     .get('/db', async (req, res) => {
         try {
           const client = await pool.connect()
